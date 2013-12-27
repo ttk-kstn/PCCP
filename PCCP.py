2a63e82e84f95abc577f55708f769d8dd82df648
ttk-kstn <ttk-kstn@server.fake>
2013-12-28 00:09:41 +0900

    Write hard-coded PCCP shell to STL file.

--- PCCP.py
+++ PCCP.py
@@ -16,8 +16,41 @@ except ImportError:
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 import STL_Writer
 
+
 def Main():
-    print "Main()!"
+    radius = 1.0
+    heights = [0.1] * 20
+    N = 12
+
+    angles = numpy.arange(0.0, 1.0, 1.0 / float(N)) * (2.0 * numpy.pi)
+    vertices = []
+    for lv in range(len(heights) + 1):
+        vertices.append([])
+        z = numpy.sum(heights[:lv])
+        for n in range(N):
+            a = angles[n]
+            if 1 == lv % 2:
+                a += 0.5 / float(N) * 2.0 * numpy.pi
+            v = (numpy.cos(a), numpy.sin(a), z)
+            vertices[lv].append(v)
+    # Connect vertices
+    faces = []
+    for lv in range(len(heights)):
+        twist = lv % 2
+        for n in range(N):
+            face = [vertices[lv + 0][n],
+                    vertices[lv + 0][(n + 1) % N],
+                    vertices[lv + 1][(n + twist) % N]]
+            faces.append(face)
+            face = [vertices[lv + 1][(n + twist + 1) % N],
+                    vertices[lv + 1][(n + twist) % N],
+                    vertices[lv + 0][(n + 1) % N]]
+            faces.append(face)
+    # Write STL file.
+    fp = open("test.stl", "wb")
+    writer = STL_Writer.Binary_STL_Writer(fp)
+    writer.add_faces(faces)
+    writer.close()
 
 if "__main__" == __name__:
     Main()
